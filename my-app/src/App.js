3617fae5eeaf66e670849fb703d85b088e828bb1
Cyryl <folkbread@gmail.com>
2022-09-13 16:58:55 +0200

    work with basic syntax react components, elements and JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,58 @@
+import {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const Header = () => {
+  return <h2>Hello World!</h2>;
+}
+
+class Field extends Component {
+  render() {
+    const holder = 'Enter here';
+    const styles = {boxShadow: "0px 0px 5px blue"};
+    return <input placeholder= {holder} type="text" style={styles}/>
+  };
+}
+
+// function Field() {
+//   const holder = 'Enter here';
+//   const styles = {
+//     boxShadow: "0px 0px 5px blue"
+//   };
+//   return <input placeholder= {holder} type="text" style={styles}/>
+// }
+
+function Btn() {
+  let content = 'Log in';
+  const styles = {
+    marginTop: "5px"
+  }
+
+  let click = false;
+
+  if (click) {
+    content = 'Clicked';
+    return <button style={styles}>{content}</button>
+  } else {
+    return <button style={styles}>{content}</button>
+  } 
+}
+
+
+  
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
+          <Header/>
+          <Field/>
+          <Btn/>
       </header>
     </div>
   );
 }
 
+export {Header};
 export default App;
