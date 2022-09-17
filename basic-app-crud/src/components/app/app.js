aaf820a4c86be4cd4aeb7881961555fa17e8537e
Cyryl <folkbread@gmail.com>
2022-09-17 13:57:34 +0200

    add header on app

---
+++ basic-app-crud/src/components/app/app.js
@@ -0,0 +1,14 @@
+import AppInfo from "../app-info/app-info";
+
+
+import "./app.css";
+
+function App() {
+    return (
+        <div className="app">
+            <AppInfo/>
+        </div>
+    )
+}
+
+export default App;
