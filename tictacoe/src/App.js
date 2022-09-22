7e20975f428d3b6f5d16b74fcdc332f075ebb9c7
Cyryl <folkbread@gmail.com>
2022-09-22 20:37:57 +0200

    learning state and circle of life components

--- tictacoe/src/App.js
+++ tictacoe/src/App.js
@@ -1,41 +1,87 @@
 import { Component } from "react";
-import Btn from "./components/btn-component/btn";
 
-function Smile({name, style}) {
-    return <p style={style}>If you see this text - smile, {name}! c:</p>
-}
+// function Smile({name, style}) {
+//     return <p style={style}>If you see this text - smile, {name}! c:</p>
+// }
+
+// class Txt extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.styles = {textShadow: "0 0 7px blue"}
+//         this.state = {birthday: 1999}
+//     }
+
+//     plusOne = () => {
+//         console.log(1999);
+//         this.setState({birthday: this.state.birthday + 1})
+//     }
+ 
+//     render() {
+//         const users = this.props.data.map((user, i) => {
+//             const {name} = user;
+//             return (
+//                 <div key={i}>
+//                     <Smile key={this.props.key} style={this.styles} name={name}/>
+//                     <button onClick={this.plusOne}>Click Me!</button>
+//                     {/* <Btn key={name}/> */}
+//                     <p>{this.state.birthday}</p>
+//                 </div>
+//             )
+//         })
+//         return (
+//             <ul>
+//                 {users}
+//             </ul>
+//         )
+//     }
+
+// }
+
+// export default Txt;
+
+// ================================
 
 class Txt extends Component {
     constructor(props) {
-        super(props)
-        this.styles = {textShadow: "0 0 7px blue"}
-        this.state = {birthday: 1999}
+        super(props);
+        this.styles = {margin: "30px auto", width: "143px", padding: "10px", textShadow: "0 0 7px red", border: "2px solid red", borderRadius: "4px", boxShadow: "0 0 20px rgba(255, 0, 0, 0.3)"}
     }
 
-    plusOne = () => {
-        console.log(1999);
-        this.setState({birthday: this.state.birthday + 1})
-    }
- 
     render() {
-        const users = this.props.data.map((user, i) => {
-            const {name} = user;
-            return (
-                <div key={i}>
-                    <Smile key={this.props.key} style={this.styles} name={name}/>
-                    <button onClick={this.plusOne}>Click Me!</button>
-                    {/* <Btn key={name}/> */}
-                    <p>{this.state.birthday}</p>
-                </div>
-            )
-        })
         return (
-            <ul>
-                {users}
-            </ul>
+            <h1 style={this.styles}>Hello!</h1>
+        )
+    }
+}
+
+class Clock extends Component {
+    constructor(props) {
+        super(props);
+        this.styles = {display: "inline", padding: "10px", textShadow: "0 0 7px blue", border: "2px solid blue", borderRadius: "4px", boxShadow: "0 0 20px rgba(0, 0, 255, 0.3)"}
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(), 1000);
+    }
+
+    // componentWillUnmount() {
+    //     clearInterval(this.timerID);
+    // }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
+
+    render() {
+        return (
+            <h2 style={this.styles}>{this.state.date.toLocaleTimeString()}</h2>
         )
     }
 
 }
 
-export default Txt;
+export {Txt};
+export default Clock;
--- tictacoe/src/index.js
+++ tictacoe/src/index.js
@@ -1,27 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Txt from "./App";
+import {Txt} from "./App";
 import "./index.css";
+import Clock from "./App";
+
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
-const obj = [
-    { name: "Kiryl Shchasny", age: 23, key: "K"},
-    { name: "Brett Pitt", age: 25, key: "B"},
-    {name: "Johny Depp", age: 32, key: "J"}
-]
+// const obj = [
+//     { name: "Kiryl Shchasny", age: 23, key: "K"},
+//     { name: "Brett Pitt", age: 25, key: "B"},
+//     {name: "Johny Depp", age: 32, key: "J"}
+// ]
 
-function Res() {
-    return (
-        <div className="app">
-            <h1>Hello World!</h1>
-            <Txt data={obj}/>
-        </div>
-    );
-}
+// function Res() {
+//     return (
+//         <div className="app">
+//             <h1>Hello World!</h1>
+//             <Txt data={obj}/>
+//         </div>
+//     );
+// }
 
 root.render(
     <React.StrictMode>
-        <Res/>
+        <div className="app">
+            <Txt/>
+            <Clock/>
+        </div>
+        {/* <Res/> */}
     </React.StrictMode>
 );
