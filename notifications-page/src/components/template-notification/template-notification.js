a5e5a670f2343ae39220d5a5b7397fdfe3b1b46d
Cyryl <folkbread@gmail.com>
2022-10-15 15:03:57 +0200

    amendments

--- notifications-page/src/components/template-notification/template-notification.js
+++ notifications-page/src/components/template-notification/template-notification.js
@@ -7,8 +7,6 @@ const phrases = ["My first tournament today!", "Chess group", "5 end-game strate
 class Notification extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-        }
 
         this.nickName = {
             color: "rgba(0, 0, 0)",
@@ -17,10 +15,7 @@ class Notification extends Component {
         }
 
         this.textNot = {
-            // position: "relative",
 
-            // left: "100px",
-            // top: "150px",
             color: "rgba(71, 71, 71, 0.8)",
             fontSize: "17px",
             width: "630px",
@@ -29,9 +24,6 @@ class Notification extends Component {
 
         this.time = {
             color: "rgba(113, 114, 115, 0.8)",
-            // position: "relative",
-            // bottom: "85px",
-            // left: "100px",
             fontWeight: "500",
             width: "630px",
             
@@ -39,10 +31,6 @@ class Notification extends Component {
         this.action = {
             color: "rgba(0, 0, 0)",
             fontWeight: "600"
-        }
-
-        this.info = {
-
         }
 
     }
