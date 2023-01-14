e8860c209b866e5e85111f94428fbbd3696c96a2
Cyryl <folkbread@gmail.com>
2023-01-14 14:55:31 +0100

    change name pipeline on index

--- web-library-app/src/index.js
+++ web-library-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./styles/style.scss";
-import App from './components/app/App';
+import App from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
